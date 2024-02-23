85c03778cf4b3308894f661cdcd667dff2b64ed4
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-23 15:40:24 +0530

    basics

---
+++ Basic Select/Japanese Cities' Attributes.sql
@@ -0,0 +1,21 @@
+--Problem Statement
+/*
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
++-------------+-------------+
+| Field       |   Type      |
++-------------+-------------+
+| ID          | NUMBER      |
+| NAME        | VARCHAR2(17)|
+| COUNTRY CODE| VARCHAR2(3) |
+| DISTRICT    | VARCHAR2(20)|
+| POPULATION  | NUMBER      |
++-------------+-------------+
+
+*/
+
+--Solution
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN'
---
+++ Basic Select/Select All.sql
@@ -0,0 +1,22 @@
+--Problem Statement
+/*
+
+Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows:
+
++-------------+-------------+
+| Field       |   Type      |
++-------------+-------------+
+| ID          | NUMBER      |
+| NAME        | VARCHAR2(17)|
+| COUNTRY CODE| VARCHAR2(3) |
+| DISTRICT    | VARCHAR2(20)|
+| POPULATION  | NUMBER      |
++-------------+-------------+
+
+*/
+
+--Solution
+
+SELECT * FROM CITY
