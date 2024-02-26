521623e5d502c20ccf0d10a3432d13726d3c9024
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-02-26 17:55:36 +0530

    basics

---
+++ Basic Select/Weather Observation Station 11.sql
@@ -0,0 +1,23 @@
+--Problem Statement
+/*
+
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+The STATION table is described as follows:
+
++-------------+-------------+
+| Field       |   Type      |
++-------------+-------------+
+| ID          | NUMBER      |
+| CITY        | VARCHAR2(21)|
+| STATE       | VARCHAR2(2) |
+| LAT_N       | NUMBER      |
+| LONG_N      | NUMBER      |
++-------------+-------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+--Solution
+SELECT DISTINCT(CITY) FROM STATION WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u') 
+OR RIGHT(CITY,1) NOT IN ('a','e','i','o','u') 
